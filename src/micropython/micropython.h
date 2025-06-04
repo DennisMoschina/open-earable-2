6f2d9ef69ae7d8eb7c7854a262bbfb5abf2379ba
Dennis <45356478+DennisMoschina@users.noreply.github.com>
2025-06-04 22:56:38 +0200

    modified micropython to compile into firmware

---
+++ src/micropython/micropython.h
@@ -0,0 +1,14 @@
+#ifndef _MICROPYTHON_IMP_H_
+#define _MICROPYTHON_IMP_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void init_mp();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // _MICROPYTHON_IMP_H_
