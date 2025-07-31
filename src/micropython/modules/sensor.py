a82855a4e11a915da21a621a013166877b1688f0
Dennis <45356478+DennisMoschina@users.noreply.github.com>
2025-08-01 01:44:42 +0200

    removed self from sensor completion handler

--- src/micropython/modules/sensor.py
+++ src/micropython/modules/sensor.py
@@ -87,7 +87,7 @@ class Sensor:
         The callback should accept a single argument, which is the SensorValue object.
         """
         self._on_data_received_cb = completion_handler
-        oe.on_data_received(self.sensor_id, self, self._on_data_received_cb)
+        oe.on_data_received(self.sensor_id, self._on_data_received_cb)
 
     def cancel_data_received(self):
         """
