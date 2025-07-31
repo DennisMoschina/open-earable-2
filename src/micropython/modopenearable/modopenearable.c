1b749f997ed8896718c90f2176a7b08efa588039
Dennis <45356478+DennisMoschina@users.noreply.github.com>
2025-08-01 01:44:42 +0200

    removed self from sensor completion handler

--- src/micropython/modopenearable/modopenearable.c
+++ src/micropython/modopenearable/modopenearable.c
@@ -17,7 +17,7 @@ static MP_DEFINE_CONST_FUN_OBJ_3(openearable_set_led_color_obj, openearable_set_
 static MP_DEFINE_CONST_FUN_OBJ_1(openearable_set_led_mode_obj, openearable_set_led_mode);
 static MP_DEFINE_CONST_FUN_OBJ_0(openearable_get_sensor_schemes_obj, openearable_get_sensor_schemes);
 static MP_DEFINE_CONST_FUN_OBJ_3(openearable_config_sensor_obj, openearable_config_sensor);
-static MP_DEFINE_CONST_FUN_OBJ_3(openearable_on_data_received_obj, openearable_on_data_received);
+static MP_DEFINE_CONST_FUN_OBJ_2(openearable_on_data_received_obj, openearable_on_data_received);
 
 static const mp_rom_map_elem_t openearable_module_globals_table[] = {
     { MP_OBJ_NEW_QSTR(MP_QSTR__name__), MP_OBJ_NEW_QSTR(MP_QSTR__openearable) },
