61b8c2d9c160143d85c1e0debe00f7e931524a95
Dennis <45356478+DennisMoschina@users.noreply.github.com>
2025-08-08 11:51:10 +0200

    src/micropython/modules/openearable.py: expose SlAddress

--- src/micropython/modules/openearable.py
+++ src/micropython/modules/openearable.py
@@ -2,4 +2,4 @@ import _openearable as oe
 from status_led import Color, LedMode, set_led_color, set_led_mode
 from parse_info import *
 from sensor import SensorValue, SensorValueGroup, SensorValueComponent, Sensor, get_sensor, init_sensors, get_sensors
-from audiofilter import set_anc_filter
+from audiofilter import set_anc_filter, SlAddress
