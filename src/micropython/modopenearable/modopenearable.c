03ea0864fdacd5126d52931b8405511dfd5868ef
Dennis <45356478+DennisMoschina@users.noreply.github.com>
2025-08-08 12:43:22 +0200

    src/micropython/modopenearable/modaudiofilter: added stubs for getting anc and eq sample rates

--- src/micropython/modopenearable/modopenearable.c
+++ src/micropython/modopenearable/modopenearable.c
@@ -21,6 +21,8 @@ static MP_DEFINE_CONST_FUN_OBJ_3(openearable_config_sensor_obj, openearable_conf
 static MP_DEFINE_CONST_FUN_OBJ_2(openearable_on_data_received_obj, openearable_on_data_received);
 static MP_DEFINE_CONST_FUN_OBJ_2(set_eq_filter_obj, set_eq_filter);
 static MP_DEFINE_CONST_FUN_OBJ_2(set_anc_filter_obj, set_anc_filter);
+static MP_DEFINE_CONST_FUN_OBJ_0(get_anc_sample_rate_obj, get_anc_sample_rate);
+static MP_DEFINE_CONST_FUN_OBJ_0(get_eq_sample_rate_obj, get_eq_sample_rate);
 
 static const mp_rom_map_elem_t openearable_module_globals_table[] = {
     { MP_OBJ_NEW_QSTR(MP_QSTR__name__), MP_OBJ_NEW_QSTR(MP_QSTR__openearable) },
@@ -32,6 +34,8 @@ static const mp_rom_map_elem_t openearable_module_globals_table[] = {
     { MP_ROM_QSTR(MP_QSTR_on_data_received), MP_ROM_PTR(&openearable_on_data_received_obj) },
     { MP_ROM_QSTR(MP_QSTR_set_anc_filter), MP_ROM_PTR(&set_anc_filter_obj) },
     { MP_ROM_QSTR(MP_QSTR_set_eq_filter), MP_ROM_PTR(&set_eq_filter_obj) },
+    { MP_ROM_QSTR(MP_QSTR_get_anc_sample_rate), MP_ROM_PTR(&get_anc_sample_rate_obj) },
+    { MP_ROM_QSTR(MP_QSTR_get_eq_sample_rate), MP_ROM_PTR(&get_eq_sample_rate_obj) },
 };
 static MP_DEFINE_CONST_DICT(openearable_module_globals, openearable_module_globals_table);
 
