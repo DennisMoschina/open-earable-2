8257e98dfba44dd93a2ea260f19c0cc57607f486
Dennis <45356478+DennisMoschina@users.noreply.github.com>
2025-08-12 15:42:24 +0200

    src/sensor_processing/sensor_parser.h: fixed signature of sensor parser

--- src/sensor_processing/sensor_parser.h
+++ src/sensor_processing/sensor_parser.h
@@ -4,9 +4,17 @@
 #include "sensor_value.h"
 #include "SensorScheme.h"
 
-struct sensor_value parse_sensor_value(const char *data, size_t length, struct SensorScheme *scheme, bool cache = true);
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+struct sensor_value parse_sensor_value(struct sensor_data sensor_data, struct SensorScheme *scheme, bool cache = true);
 
 void clear_sensor_value_cache(struct SensorScheme *scheme);
 void clear_all_sensor_value_caches(void);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif // _SENSOR_PARSER_H
